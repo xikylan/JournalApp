c8ff14094b0c1a058940b446a1600eec371297c3
xiateserenity <xiateserenity@gmail.com>
2020-05-08 10:19:58 -0500

    Initial Commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Lan Nguyen on 5/8/20.
+//  Copyright © 2020 Lan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
